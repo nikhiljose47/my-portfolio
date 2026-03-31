908b398d3c5426291521cce15b485a307e9db952
N <n47@github.com>
2026-04-01 03:50:50 +0530

    content updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,6 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
   ],
 };
